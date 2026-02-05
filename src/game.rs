9f07aff4b5356f65162e1c48a24a6cfb0014299c
matej-kotrba <matejkotrba.skola@gmail.com>
2026-02-05 22:29:08 +0100

    refactor

---
+++ src/game.rs
@@ -0,0 +1,23 @@
+pub struct MainMenuData {}
+pub struct CampData {}
+pub struct ShopData {}
+pub struct DungeonData {}
+
+pub enum Screen {
+    MainMenu(MainMenuData),
+    Camp(CampData),
+    Shop(ShopData),
+    Dungeon(DungeonData),
+}
+
+pub struct Game {
+    pub screen: Screen,
+}
+
+impl Game {
+    pub fn new() -> Self {
+        Game {
+            screen: Screen::MainMenu(MainMenuData {}),
+        }
+    }
+}
